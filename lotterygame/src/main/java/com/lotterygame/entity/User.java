d8eb0ca349b50837a0caead95959e4abb2d4b9c5
Geza Biro <birogeza@gmail.com>
2021-02-01 09:00:08 +0100

    lotterygame

---
+++ lotterygame/src/main/java/com/lotterygame/entity/User.java
@@ -0,0 +1,130 @@
+package com.lotterygame.entity;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+//@Table( name="users" )
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column( unique=true, nullable=false )
+    private String userName;
+    @Column( nullable=false )
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String vehicle;
+    private String email;
+    private String phone;
+//    @ManyToMany( cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+//    @JoinTable(
+//            name = "users_games",
+//            joinColumns = {@JoinColumn(name="user_id")},
+//            inverseJoinColumns = {@JoinColumn(name="game_id")}
+//    )
+
+    //private List<Game> games = new ArrayList<Game>();
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public User(String userName, String password, String firstName, String lastName, String gender, String vehicle, String email, String phone) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.vehicle = vehicle;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public User(){}
+
+
+}
+
